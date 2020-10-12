e2ee9cf9a534fc72461dba676f081f029bf6fc24
Sam Radford <smr1292@yahoo.com>
2020-10-12 16:33:28 -0400

    changed the database enum back to the correct collection

--- src/common/database/DatabaseCollection.enum.ts
+++ src/common/database/DatabaseCollection.enum.ts
@@ -1,5 +1,5 @@
 export enum DatabaseCollection {
     TestCollectionOne = 'test_collection_one',
     TestCollectionAnime = 'test_collection_anime_one',
-    CompletedAnime = 'another_test',
+    CompletedAnime = 'completed_anime',
 }
